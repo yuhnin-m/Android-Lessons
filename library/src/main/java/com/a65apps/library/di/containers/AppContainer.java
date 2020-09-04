81ae66ce651417a439d21ce783dc85f63296cb06
Yukhnin Mikhail <yuhnin.m@ya.ru>
2020-09-04 10:54:04 +0400

    Переведены контейнеры

--- library/src/main/java/com/a65apps/library/di/containers/AppContainer.java
+++
@@ -1,9 +0,0 @@
-package com.a65apps.library.di.containers;
-
-public interface AppContainer {
-    PersonDetailsContainer plusPersonDetailsComponent();
-    PersonListContainer plusPersonListComponent();
-    BirthdayReminderContainer plusBirthdayReminderComponent();
-    PersonLocationContainer plusPersonLocationComponent();
-
-}
--- library/src/main/java/com/a65apps/library/di/containers/PersonListContainer.java
+++
@@ -1,7 +0,0 @@
-package com.a65apps.library.di.containers;
-
-import com.a65apps.library.ui.fragments.PersonListFragment;
-
-public interface PersonListContainer {
-    void inject(PersonListFragment contactListFragment);
-}
